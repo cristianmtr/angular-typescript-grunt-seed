13aaed2c727456e8fd5eb79191ca5399d2b20680
cristian <cristi.mtr@gmail.com>
2016-05-02 23:11:58 +0200

    added jquery; index html refs local libs

--- ts/controllers/mainCtrl.ts
+++ ts/controllers/mainCtrl.ts
@@ -4,6 +4,11 @@ module finalassignment {
     export class mainCtrl {
         constructor(private $scope) {
             $scope.testVar = "X";
+            $scope.otherVar = 222;
+            $scope.testFunc = function () {
+                $scope.otherVar ++;
+                console.log($scope.otherVar);
+            }
         }
     }
 }
