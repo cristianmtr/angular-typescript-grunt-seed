faecc5f7bd3268d777644b34ec92c3a9e33618e4
Cristian Mtr <cristianmtr@users.noreply.github.com>
2016-05-10 13:01:09 +0200

    Update tsd.d.ts
    fix: reference path

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
+/// <reference path="../ts/services/Database.ts" />
+/// <reference path="../ts/controllers/MainController.ts" />
+/// <reference path="../ts/models/Task.ts" />
